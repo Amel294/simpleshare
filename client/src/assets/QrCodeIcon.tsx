d2e405f1fa46eb2163987eeaacf0d2e711d7ac7a
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-02 15:17:03 +0530

    fix(client): resolve TypeScript implicit 'any' type errors in icon components and remove unused react.svg

--- client/src/assets/QrCodeIcon.tsx
+++ client/src/assets/QrCodeIcon.tsx
@@ -1,10 +1,10 @@
-const QrCodeIcon = (props) => (
+const QrCodeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     {...props}
   >
-    <path d="M5 15v2H3v-2Zm-2 6h2v-2H3Zm2-4v2h2v-2Zm4-6H7v2H5v2h4Zm-6 0v2h2v-2Zm6 8H7v2h5v-2h-1v-2H9Zm4-15h-2v2h2Zm-2 7h2V8h-2ZM4 9a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1Zm1-2h2V5H5Zm16-3v8h-2V9h-3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1Zm-2 1h-2v2h2Zm2 11v4a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-3h-4v-4h2v2h1v-4h2v4h4a1 1 0 0 1 1 1Zm-2 1h-2v2h2Z" />
+    <path d="M5 15v2H3v-2Zm-2 6h2v-2H3Zm2-4v2h2v-2Zm4-6H7v2H5v2h4Zm-6 0v2h2v-2Zm6 8H7v2h5v-2h-1v-2H9Zm4-15h-2v2h2Zm-2 7h2V8h-2ZM4 9a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4Zm16-2h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1ZM4 18v2h2v-2H4ZM3 8h2V6H3Zm12-2h-2V4h2Zm0 6h-2v2h2Zm2 8h-2v-2h2ZM4 12h2v-2H4Zm16 0h-2v-2h2Zm-8-4h2V4h-2Zm0 6h2v-2h-2Zm2-2h-2v-2h2Z" fill="currentColor"/>
   </svg>
-)
+);
 export default QrCodeIcon
